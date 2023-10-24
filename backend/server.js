05fb77a8d523b6f02b29bbf8ec39f473a3b73a7a
jainvedant392 <jainvedant392@gmail.com>
2023-10-24 22:02:33 +0530

    added cors

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const colors = require('colors');
 const dotenv = require('dotenv').config();
 const port = process.env.PORT || 3001;
+const cors = require('cors');
 const connectDB = require('./config/db');
 const {errorHandler} = require('./middlewares/errorMiddleware');
 
@@ -9,6 +10,7 @@ connectDB();
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.status(200).send({message: "welcome to the todo - app - api!"});
