854a275b4ee7aae5245572104eab5cd7a1bd1646
jainvedant392 <jainvedant392@gmail.com>
2023-10-24 22:22:12 +0530

    modified cors origin

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
 const corsOptions = {
-    origin: 'http://localhost:5173',
+    origin: 'http://localhost:5174',
     methods: 'GET,POST,PUT,DELETE',
     optionsSuccessStatus: 200,
     credentials: true,
