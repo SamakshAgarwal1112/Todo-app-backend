2600eae7cbc640d933306a6591506d533cb30480
jainvedant392 <jainvedant392@gmail.com>
2023-10-25 13:36:05 +0530

    changed cors origin

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
 const corsOptions = {
-    origin: '*',
+    origin: ["http://localhost:5173", "http://localhost:5174"],
     methods: 'GET,POST,PUT,DELETE',
     optionsSuccessStatus: 200,
     credentials: true,
