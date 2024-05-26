4ba3f7cd266ae164095709192ef723c86a1b962d
jainvedant392 <jainvedant392@gmail.com>
2024-05-26 12:19:43 +0530

    modified cors origins

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const corsOptions = {
   origin: [
     "http://localhost:5173",
     "http://localhost:5174",
-    "https://todo-app-nbds-rqnm-lsobmc2bp-jainvedant392s-projects.vercel.app/",
+    "https://todo-app-nbds-rqnm-lsobmc2bp-jainvedant392s-projects.vercel.app",
   ],
   methods: "GET,POST,PUT,DELETE",
   optionsSuccessStatus: 200,
